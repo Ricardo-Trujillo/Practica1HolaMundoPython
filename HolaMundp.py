2b1dc20578b6a26955b9cfa5e8d1d957074853ca
Ricardo Trujillo <ricardo.donovan.lopez.trujillo@uabc.edu.mx>
2022-08-18 12:18:51 -0700

    Taller1 LPP
    Hola mundo del taller 1 de LPP

---
+++ HolaMundp.py
@@ -0,0 +1,8 @@
+from ctypes.wintypes import PCHAR
+
+
+print("Hola Mundo desde Python\n")
+
+#commit solo guarda el archivo en la PC
+#commit and push guarda el archivo y empuja los cambios al git
+#commit and sync guarda el archivo en la pc, se guarda y si hay alguna otra actualizacion lo descarga
